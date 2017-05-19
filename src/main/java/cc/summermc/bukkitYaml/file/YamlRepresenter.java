37c08a583631751e5ce25b7170e00a6bcf29a07b
l89669 <l89669@hotmail.com>
2017-05-20 03:50:06 +0800

    0.1-pre

---
+++ src/main/java/cc/summermc/bukkitYaml/file/YamlRepresenter.java
@@ -0,0 +1,36 @@
+package cc.summermc.bukkitYaml.file;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.yaml.snakeyaml.nodes.Node;
+import org.yaml.snakeyaml.representer.Representer;
+
+import cc.summermc.bukkitYaml.ConfigurationSection;
+
+public class YamlRepresenter extends Representer {
+
+    public YamlRepresenter() {
+        this.multiRepresenters.put(ConfigurationSection.class, new RepresentConfigurationSection());
+        //this.multiRepresenters.put(ConfigurationSerializable.class, new RepresentConfigurationSerializable());
+    }
+
+    private class RepresentConfigurationSection extends RepresentMap {
+        @Override
+        public Node representData(Object data) {
+            return super.representData(((ConfigurationSection) data).getValues(false));
+        }
+    }
+
+    /*private class RepresentConfigurationSerializable extends RepresentMap {
+        @Override
+        public Node representData(Object data) {
+            ConfigurationSerializable serializable = (ConfigurationSerializable) data;
+            Map<String, Object> values = new LinkedHashMap<String, Object>();
+            values.put(ConfigurationSerialization.SERIALIZED_TYPE_KEY, ConfigurationSerialization.getAlias(serializable.getClass()));
+            values.putAll(serializable.serialize());
+
+            return super.representData(values);
+        }
+    }*/
+}
